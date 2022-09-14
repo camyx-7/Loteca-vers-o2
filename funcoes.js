ed7a729961d9d27a4b393573680d3b370c03a642
camyx-7 <camillylimabarrios@gmail.com>
2022-09-14 10:14:46 -0300

    fim do js

--- funcoes.js
+++ funcoes.js
@@ -11,25 +11,35 @@ function sortearNumeros(){
     } else {
         i = i - 1;
     }
-        numSorteados[i] = num
 }
-    console.log(numSorteados)
+    document.getElementById("sorteados").innerHTML= numSorteados;
+    contarAcertos()
 }
 
-function lerNumero(){
-    if (value < 1){
+function lerNumero(value, pos){
+    let num = Number(value)
+
+    if (num < 1){
         alert("Número não poder zero ou negativo! Digite novamente:")
-    } else if(value > 60){
+    } else if(num > 60){
         alert("Número não pode ser maior que 60! Digite Novamente:")
-    } else if(numEscolhidos.includes(value)){
+    } else if(numEscolhidos.includes(num)){
         alert("Número repetido! Digite novamente:")
     } else{
-    numEscolhidos[pos] = value
+    numEscolhidos[pos] = num
     console.log(numEscolhidos)
     
     }
 }
-
+function contarAcertos(){
+    let cont = 0
+    numEscolhidos.forEach(function(value, index){
+        if(numSorteados.includes(value)){
+            cont++;
+        }
+    })
+document.getElementById("acertos").innerHTML = cont;
+}
 //function verAcertos(){
     //numEscolhidos.forEach(function(valor))
 //}
